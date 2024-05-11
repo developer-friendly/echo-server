4fe4e340509349e6f8192bbf9f0cae261a6a64f2
Meysam Azad <MeysamAzad81@gmail.com>
2024-05-11 11:28:41 +0700

    chore: rewrite with actix-web
    it's the easiest apparently & with the least drama
    
    I'm also more familiar with it

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,65 @@
-use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Json, Router,
-};
+use actix_web::{get, web, App, HttpRequest, HttpServer};
 
 mod types;
-use types::{User, CreateUser};
+use types::EchoInfo;
+
+#[get("/")]
+async fn index(req: HttpRequest) -> web::Json<EchoInfo> {
+    tracing::debug!("Received request: {:?}", req);
+    let headers = req
+        .headers()
+        .iter()
+        .map(|(key, value)| (key.to_string(), value.to_str().unwrap().to_string()))
+        .collect();
+    tracing::debug!("Headers: {:?}", headers);
+    let cookies = req
+        .cookies()
+        .iter()
+        .flat_map(|cookies| {
+            cookies
+                .iter()
+                .map(|cookie| (cookie.name().to_string(), cookie.value().to_string()))
+        })
+        .collect();
+    tracing::debug!("Cookies: {:?}", cookies);
+    let query_params = req
+        .query_string()
+        .split('&')
+        .filter_map(|param| {
+            let mut split = param.split('=');
+            match (split.next(), split.next()) {
+                (Some(key), Some(value)) => Some((key.to_string(), value.to_string())),
+                _ => None,
+            }
+        })
+        .collect();
+    tracing::debug!("Query params: {:?}", query_params);
+    let client_ip = req.connection_info().peer_addr().unwrap().to_string();
+    // get server ip from the request
+    let server_hostname = req.connection_info().host().to_string();
+
+    let echo = EchoInfo::new(
+        req.uri().to_string(),
+        headers,
+        None,
+        query_params,
+        cookies,
+        client_ip,
+        server_hostname,
+    );
+
+    tracing::info!("Echo: {:?}", echo);
+    web::Json(echo)
+}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/users", post(create_user));
+    tracing::info!("Starting server at 0.0.0.0:3000");
 
-    let addr = "0.0.0.0:3000";
-    tracing::info!("listening on {:?}", &addr);
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 3000))?
+        .run()
+        .await
 }
-
-async fn root() -> &'static str {
-    "Hello, World!"
-}
-
-async fn create_user(
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    let user: User = payload.into();
-
-    (StatusCode::CREATED, Json(user))
-}
-
--- src/types.rs
+++ src/types.rs
@@ -1,29 +1,46 @@
-use serde::{Deserialize, Serialize};
+use hostname::get;
+use serde::{Serialize};
+use std::collections::HashMap;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-type UUID = String;
-
-#[derive(Deserialize)]
-pub struct CreateUser {
-    username: String,
+#[derive(Debug, Serialize)]
+pub struct EchoInfo {
+    epoch: u64,
+    time_isoformat: String,
+    hostname: String,
+    server_hostname: String,
+    request_uri: String,
+    headers: HashMap<String, String>,
+    body: Option<String>,
+    query_params: HashMap<String, String>,
+    cookies: HashMap<String, String>,
+    client_ip: String,
 }
 
-#[derive(Serialize)]
-pub struct User {
-    id: UUID,
-    username: String,
-}
-
-impl From<CreateUser> for User {
-    fn from(create_user: CreateUser) -> Self {
-        User::new(create_user.username)
-    }
-}
-
-impl User {
-    pub fn new(username: String) -> Self {
-        User {
-            id: uuid::Uuid::new_v4().to_string(),
-            username,
+impl EchoInfo {
+    pub fn new(
+        request_uri: String,
+        headers: HashMap<String, String>,
+        body: Option<String>,
+        query_params: HashMap<String, String>,
+        cookies: HashMap<String, String>,
+        client_ip: String,
+        server_hostname: String,
+    ) -> Self {
+        Self {
+            request_uri,
+            headers,
+            body,
+            query_params,
+            cookies,
+            client_ip,
+            server_hostname,
+            epoch: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+            time_isoformat: chrono::Utc::now().to_rfc3339(),
+            hostname: get().unwrap().into_string().unwrap(),
         }
     }
 }
